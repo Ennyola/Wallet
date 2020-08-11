8cb0a89ff93b74fc4a05593c4f4e21bf78863ddf
Ennyola <medunoyeeni@gmail.com>
2020-08-11 03:29:15 +0100

    Transactions page modified

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -6,15 +6,38 @@ import SideBar from './Sidebar'
 
 const Transactions = (props)=>{
 
-    const {data} = useQuery(query)
+    const {data, loading, error} = useQuery(query)
+    
+    
+    const displayTransactions = ()=>{
+          if (data){
+            const {transactions} = data
+            return transactions.map(({id,moneySaving, moneySpending, timeOfTransaction})=>{
+                return(
+                  <div className = "transaction" key = {id}>
+                   Money Added - {moneySaving} <br/>
+                   Money Deducted - {moneySpending} <br/>
+                   Time of Transaction - {timeOfTransaction} <br/>
+                  </div>
+                )         
+
+              }
+            ) 
+          }
+       }
     
 
     return(
         <div className = "transactions">
             <Header/>
             <SideBar/>
-            <div className = "">
+            <div className = "body">
+              <h4>Transactions</h4>
 
+              <div className = "transaction-list">
+                {displayTransactions()}
+              </div>
+              
             </div>
         </div>
     )
