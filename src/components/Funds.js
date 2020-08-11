8ddb1c40100e05e30c97e57faccfca469f80194c
Ennyola <medunoyeeni@gmail.com>
2020-08-11 04:16:04 +0100

    modified dashboard styles

--- src/components/Funds.js
+++ src/components/Funds.js
@@ -7,12 +7,18 @@ import SideBar from './Sidebar'
 const Funds =(props)=>{
 
     const {data, error, loading} = useQuery(query)
+
+
+
     return(
         <div className = "funds">
             <Header/>
             <SideBar/>
-            <div className = "">
-
+            <div className = "body">
+              <h4>Funds</h4>
+              <div className = "my-funds">
+              
+              </div>
             </div>
         </div>
     )
