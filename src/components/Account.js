5d16242bbbeb6ab0ecb8c134a0c5e21148dc3260
Ennyola <medunoyeeni@gmail.com>
2020-08-23 01:26:28 +0100

    Added image hover event

--- src/components/Account.js
+++ src/components/Account.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React,{createRef} from 'react';
+import {MDBPopover} from 'mdbreact'
 
 import Header from './Header'
 import Sidebar from './Sidebar'
@@ -6,17 +7,37 @@ import Sidebar from './Sidebar'
 import photo from '../public/images/opened-laptop-on-table-1128240.jpg'
 const Account = ()=>{
 
+    const overlayRef = createRef()
+
+    const onMouseEnter=(e)=>{
+        overlayRef.current.style.display = "inline"
+        
+        
+    }
+    const onMouseLeave = (e)=>{
+        overlayRef.current.style.display = "none"
+    }
+
+    const onClick = (e)=>{
+
+    }
+
     return(
         <div className = "account">
             <Header/>
            
             <div className="body">
-                <div className = "account-image">
-                    <img src={photo} alt=""/>
+                <div onMouseEnter= {onMouseEnter} onMouseLeave = {onMouseLeave} className = "account-image">
+                    <img src={photo} alt=""/> 
+                    <div ref= {overlayRef} onClick = {onClick} className = "img-overlay">
+                        <span>
+                            Click here to 
+                            <br/>
+                            change Profile photo
+                        </span>
+                    </div>
                 </div>
-                <div>
-                    Email
-                </div>
+                
                 
             </div>
 
