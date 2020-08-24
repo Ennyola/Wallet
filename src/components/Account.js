8040e9cb9dd4f1cf39fc06259b69921217c02275
Ennyola <medunoyeeni@gmail.com>
2020-08-24 23:50:49 +0100

    Still battling errors

--- src/components/Account.js
+++ src/components/Account.js
@@ -1,5 +1,6 @@
-import React,{createRef} from 'react';
+import React,{createRef, useState} from 'react';
 import {MDBPopover} from 'mdbreact'
+import {Image, Video, Transformation, CloudinaryContext} from 'cloudinary-react';
 
 import Header from './Header'
 import Sidebar from './Sidebar'
@@ -8,35 +9,81 @@ import photo from '../public/images/opened-laptop-on-table-1128240.jpg'
 const Account = ()=>{
 
     const overlayRef = createRef()
+    const [hovered, setHover] = useState(false)
+    const [clicked, setClicked] = useState(false)
 
-    const onMouseEnter=(e)=>{
-        overlayRef.current.style.display = "inline"
-        
-        
+    //setHover checks if the image div was hovered on. 
+    //If set to false, the overlay div would not show, 
+    //but set to true, will make it show
+    const hoverOverImage=(e)=>{   
+            setHover(true)
+            
     }
-    const onMouseLeave = (e)=>{
-        overlayRef.current.style.display = "none"
+    const leaveImage = (e)=>{
+        if(!clicked){
+            setHover(false) 
+        }
     }
 
-    const onClick = (e)=>{
-        
+    const setClick = (e)=>{
+        setClicked(true)
     }
+
+    const clickBody = (e)=>{
+       if (clicked){
+           setClicked(true)
+           setHover(false)
+       }
+  
+    }
+
+   
+
+    const imageisClicked = ()=>{
+        if (hovered){
+                return(
+                    <MDBPopover
+                    placement = "right"
+                    popover
+                    clickable
+                    domElement
+                    id="popper1"
+                    >
+                        <div ref= {overlayRef} onClick = {setClick}  className = "img-overlay">
+                            <span>
+                                Click here to 
+                                <br/>
+                                change Profile photo
+                                
+                            </span>
+                        </div>
+                        <div>
+                            <a href="">View Photo</a>
+                            <a href="">Take Photo</a>
+                            <a href="">Upload Photo</a>
+                            <a href="">Remove Photo</a>
+                        </div>
+                    </MDBPopover>
+                )
+            }
+    }
+
+    
 
     return(
-        <div className = "account">
+        <div className = "account" onClick = {clickBody}>
             <Header/>
            
             <div className="body">
-                <div onMouseEnter= {onMouseEnter} onMouseLeave = {onMouseLeave} className = "account-image">
+                <div onMouseEnter = {hoverOverImage} onMouseLeave = {leaveImage} className = "account-image">
                     <img src={photo} alt=""/> 
-                    <div ref= {overlayRef} onClick = {onClick} className = "img-overlay">
-                        <span>
-                            Click here to 
-                            <br/>
-                            change Profile photo
-                        </span>
-                    </div>
+                    {imageisClicked()}
                 </div>
+
+                {/* <Image cloudName="awploder" publicId="sample" width="300" crop="scale" />
+
+                 */}
+                
                 
                 
             </div>
